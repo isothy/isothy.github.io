0b52c50149c30a007b57ade971e3c6738c1920e1
isothy <isothy@yandex.com>
2019-09-27 22:49:02 +0300

    commit

--- js/add-content.js
+++ js/add-content.js
@@ -1,16 +1,9 @@
-var today = new Date();
-var hourNow = today.getHours();
-console.log(hourNow);
-var greeting;
+var price, quantity, total;
 
-if (hourNow > 18) {
-  greeting = 'Good Evening!';
-} else if (hourNow > 12) {
-  greeting = 'Good Afternoon!';
-} else if (hourNow > 0) {
-  greeting = 'Good Morning!';
-} else {
-  greeting = 'Welcome!';
-}
+price = 5;
+quantity = 14;
 
-document.write('<h3>' + greeting + '</h3>');
+total = price * quantity;
+
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
